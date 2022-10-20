5ca2ad60ad28c770e6a7119526ad2ee03d885a44
Filipe <116228977+FilipeMell0@users.noreply.github.com>
2022-10-20 00:53:10 +0000

    Print de variáveis (nome e idade).

--- main.py
+++ main.py
@@ -3,6 +3,14 @@
 #print() = comando de saído
 print("Alô mundo!")
 
+#Guardar string 
+nome = "Filipe Mello"
+idade = 18
+
+#Exibir nome dentro da variável
+print("Meu nome é", nome,"Tenho",idade,"anos")
+
+
 '''
 Comentario em bloco
 '''
