984cbf0acd0ccf5b08c136c01229b9c13f2643ec
Filipe <116228977+FilipeMell0@users.noreply.github.com>
2022-10-27 00:34:44 +0000

    Aprendendo condicionais

--- main.py
+++ main.py
@@ -3,8 +3,25 @@ nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
 
 #comando de saída
-print(f"Seu nome é {nome} e você tem {idade} anos")
+print(f"\nSeu nome é {nome} e você tem {idade} anos")
 
 # mostrar o dobro da idade
 dobro = idade * 2
-print(f"O dobro da sua idade é: {dobro}")
+print(f"O dobro da sua idade é: {dobro}\n")
+
+#Estrutura condicional = if
+if idade>=18:
+  print(f"Você pode beber ou dirigir!\n")
+
+else:
+  print(f"Infelizmente vai ficar só na carona e no refri\n")
+
+#E se eu quisessem perguntar o gênero (M = Masculino e F = Feminino)
+#Mostre (...e você também precisa/precisou prestar o serviço militar obrigatório)
+
+genero = input("Qual seu gênero? ")
+
+if idade >= 18 and genero == "m" or "M":
+  print("Você prestou o serviço militar!")
+else:
+  print("Você é mulher ou menor de idade :)")
