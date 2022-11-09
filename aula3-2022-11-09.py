be333d65a11dd6ed4addc69654acf503942d7473
Filipe <116228977+FilipeMell0@users.noreply.github.com>
2022-11-09 23:12:39 +0000

    Atualizações no código

---
+++ aula3-2022-11-09.py
@@ -0,0 +1,35 @@
+print("Início da aula 3 (09/11/2022)")
+
+contador = 0
+
+#Exibir de 1 a 10 repetidamente
+while(contador <=10):
+  print(contador)
+  contador+=1
+
+#Laço for 
+fruta = "morango"
+print(fruta)
+
+#Lista 
+frutas = ["morango", "laranja", "pêra"]
+
+#Quero exibir apenas a 3 fruta
+print(frutas[2])
+
+#Exibir quantas frutas tem na lista
+print(len(frutas)) #length = tamanho
+
+#Incluir fruta nova
+frutas.append("manga")
+
+print(len(frutas)) #length = tamanho
+
+print(frutas)
+
+
+i = 0
+
+while(i < 4):
+  print(frutas[i])
+  i+=1
