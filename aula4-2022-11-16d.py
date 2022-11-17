858d3371499b5a2f907507a4a9b355d0d0e4144a
Filipe <116228977+FilipeMell0@users.noreply.github.com>
2022-11-17 01:12:14 +0000

    Adicionei um vilão e adicionei o aquaman como vilão sem querer

--- aula4-2022-11-16d.py
+++ aula4-2022-11-16d.py
@@ -4,18 +4,18 @@ con, cur = bd.conectar()
 
 nome = input("Informe o nome do herói/vilão:")
 nome_civil = input("Informe o nome civil do herói/vilão (sua identidade secreta):")
-tipo_numerico = input("Tecle 1 para herói(na) ou 2 para vilã(o)")
+tipo_numerico = input("Tecle 1 para herói(na) ou 2 para vilã(o): ")
 
 #consulta para o valor max usado no banco
 
-sql = "SELECT MAX(pessoa_id)+1 FROM pessoa"
+sql = "SELECT MAX(pessoa_id)+1 FROM pessoas"
 cur.execute(sql)
 pessoa_id = cur.fetchone()[0]
 
 if tipo_numerico == "1":
   tipo = "Herói(na)"
 else:
-  tipo + "Vilã(o)"
+  tipo = "Vilã(o)"
 
 sql = f"INSERT INTO pessoas (pessoa_id, nome, nome_civil, tipo) VALUES ({pessoa_id}, '{nome}', '{nome_civil}', '{tipo}')"
 
